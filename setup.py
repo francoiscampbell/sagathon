9c85cf89a34982db13cf4d08987b953cb1a8bd27
Francois Campbell <campbell.francois@gmail.com>
2018-12-10 21:46:19 -0800

    bookkeeping

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sagathon',
+    version='0.0.1',
+    url='https://github.com/francoiscampbell/sagathon.git',
+    author='Francois Campbell',
+    author_email='campbell.francois@gmail.com',
+    description='Redux-saga, without redux, for python',
+    packages=find_packages(),    
+    install_requires=[],
+)
